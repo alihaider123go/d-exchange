381603501d5fc0ae568183278a7949264e103d7b
Ali Haider <alihaider123go@gmail.com>
2024-10-15 15:04:26 +0500

    Add hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
+import {IoCloseOutline} from "react-icons/io5";
 
 const Hero = ({setInputAmount, setLoader, setOpenToken, LOAD_TOKEN, token_1, token_2, setToken_1, setToken_2,swap}) => {
-  return <div>Hero</div>;
+    
+    const reset = ()=>{
+        setToken_1("");
+        setToken_2("");
+    }
+    return (
+        <div className="banner" id="home" style={{backgroundImage:'url("assets/img/banner-bg.png")'}}>
+            <div className="illustration">
+                <img src="assets/img/banner-bg-1.png" alt="banner-bg-1" className="one" />
+                <img src="assets/img/banner-bg-2.png" alt="banner-bg-2" className="two" />
+                <img src="assets/img/banner-map.png" alt="banner-map" className="three" />
+            </div>
+            <div className="hero-area">
+                <div className="container">
+                    <div className="row align-items-center justify-content-between">
+                        <div className="col-xl-7 col-lg-6">
+                            <div className="banner-content wow fadeInUp" data-wow-duration="0.5s" data-wow-delay="0.3s">
+                                <h3 className="subtitle">Fast & Convenient</h3>
+                                <h1 className="head">Cryptocurrency Exchange</h1>
+                                <p className="text">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, quisquam. </p>
+                            </div>
+                        </div>
+                        <div className="col-xl-4 col-lg-6 wow fadeInRightBig" data-wow-duration="0.3s" data-wow-delay="0.5s">
+                            <div className="exchange">
+                                <h5 className="ex-head">
+                                    Cryptocurrency Exchange
+                                </h5>
+                                <div className="exchange-box">
+                                    <div className="selector">
+                                        <p className="text">
+                                            Your Change
+                                        </p>
+                                        <div className="coin">
+                                            <span>
+                                                {token_1?.symbol}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="form-group">
+                                            <span onClick={()=>setOpenToken(true)}>Open</span>
+                                            <input type="text" className="form-control" placeholder={token_1?.symbol || "Select"} onChange={(e)=>setInputAmount(e.target.value)}/>
+                                        </div>
+                                    </div>
+
+                                    {
+                                        token_1?(
+                                            <span className="rate">{`Balance: ${token_1?.balance.slice(0,10)}`} {token_1?.symbol}</span>
+                                        ):(
+                                            ""
+                                        )
+                                    }
+                                </div>
+                                <a className="rotate">
+                                    <img src="assets/img/exchange-img.png" alt="exchange-rotate-icon" onClick={()=>reset()}/>
+                                </a>
+                                <div className="exchange-box">
+                                    <div className="selector">
+                                        <p className="text">
+                                            You Get
+                                        </p>
+                                        <div className="coin">
+                                            <span>
+                                                {token_2?.symbol}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="form-group">
+                                            <span onClick={()=>setOpenToken(true)}>Open</span>
+                                            <input type="text" className="form-control" placeholder={token_2?.symbol || "Select"} onClick={()=>setOpenToken(true)}/>
+                                        </div>
+                                    </div>
+
+                                    {
+                                        token_2?(
+                                            <span className="rate">{`Balance: ${token_2?.balance.slice(0,10)}`} {token_2?.symbol}</span>
+                                        ):(
+                                            ""
+                                        )
+                                    }
+                                </div>    
+                                <a onClick={()=>swap()} className="button button-1">Exchange</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Hero;
